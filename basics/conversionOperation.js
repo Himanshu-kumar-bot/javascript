9b95804fb5ac190911aaede917ad61b9bb944aa6
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-26 17:51:51 +0000

    conversions

---
+++ basics/conversionOperation.js
@@ -0,0 +1,18 @@
+ let score="23aa"
+  let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//"33" -> 33
+// "33aab" -> NaN
+//true -> 1; false -> 0
+
+let isLoggedIn="himanshu"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+
+//1 ->true 0->false
+//"" -> false
+//"himanshu" ->true
