40699b4585409e45ae91adae995759838dc88ccb
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-26 17:32:38 +0000

    datatypes

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,18 @@
- console.log("hello");
+  const accountId=153
+  let accountEmail="himanshu@gmail.com";
+  var accountPassword="1234"
+  acountCity="jaipur"
+//   accountId=12
+/*
+prefer not to use Var because of issue in block scope and functional scope
+*/
+
+
+
+
+accountEmail=" Hnshu@gmail.com";
+accountPassword="566666666666"
+acountCity=" begaluru"
+  console.log(accountId);
+  console.table([accountId, accountEmail,accountPassword,acountCity])
+ 
