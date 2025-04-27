38c82e32f13b0b312baa7b5883442781ae90aef9
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-27 05:53:10 +0000

    operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,7 +1,7 @@
  let score="23aa"
   let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 //"33" -> 33
@@ -10,9 +10,42 @@ console.log(valueInNumber)
 
 let isLoggedIn="himanshu"
 let booleanisLoggedIn=Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 
 
 //1 ->true 0->false
 //"" -> false
 //"himanshu" ->true
+
+
+
+//  *************************************operation********************************************
+  let value=3
+  let negValue=-value
+  // console.log(negValue);
+
+
+  // console.log(2+2);
+  // console.log(+true);
+  // console.log(+"");
+
+
+// console.log("02">1);
+// console.log("2"<"1");
+
+
+
+
+
+  //
+  console.log(null==0);
+  console.log(null>0);
+  console.log(null>=0);   //comparison convert null to a number, treating it as 0. That'why it gives true
+  
+  
+  
+  console.log(undefined>0);
+  console.log(undefined ==0);
+  console.log( undefined>=0);
+  
+//=== it also check datatype
