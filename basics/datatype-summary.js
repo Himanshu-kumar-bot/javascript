c04ff79675356acc35d988a028512992e5d8da83
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-27 07:51:57 +0000

    Summary

--- basics/datatype-summary.js
+++ basics/datatype-summary.js
@@ -35,3 +35,23 @@ let introduction={
 }
 console.log(typeof introduction);
 myfunction()
+
+
+/*
+Type	         Result
+Undefined    	"undefined"
+Null	        "object" (reason)
+Boolean     	"boolean"
+Number      	"number"
+BigInt	        "bigint"
+String	        "string"
+Symbol       	"symbol"
+Function (implements [[Call]] in ECMA-262 terms; classes are functions as well)	"function"
+Any other object  "object"
+
+
+
+
+
+
+*/
