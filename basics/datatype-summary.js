0a1c49872483ab065f282eb15fc91a4217108fc3
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-27 07:48:42 +0000

    Summary

---
+++ basics/datatype-summary.js
@@ -0,0 +1,37 @@
+ // Primitive 
+ // 7-types => String , Number , Boolean , null , undefined , BigInt , Symbol
+
+
+ //Reference (Non-primitive)
+ //  => Array , Functions , Object
+
+
+ /*
+JavaScript is a dynamically typed language.
+This means that the type of a variable is checked during runtime, 
+as opposed to static typing where types are checked during compile time. In JavaScript,
+you do not need to explicitly declare the data type of a variable;
+it's determined automatically based on the assigned value. 
+This allows a variable to hold values of different types throughout its lifespan in a program.
+
+
+
+ */
+
+const score=Symbol('123')
+const id=Symbol('123')
+console.log(score===id);
+
+
+let heros=["shaktiman","ironman","lokiii", ];
+const myfunction=function(){
+console.log("hello world");
+}
+
+let introduction={
+    name:"himanshu",
+    age:18,
+    hieght:5.6
+}
+console.log(typeof introduction);
+myfunction()
