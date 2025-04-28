a5732a9d058f0edb3768a6cdba560c11f5f35546
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-28 18:09:24 +0000

    date and time

---
+++ basics/Date_and_Time.js
@@ -0,0 +1,24 @@
+ let myDate=new Date()
+
+
+ console.log(myDate);
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toISOString ());
+ console.log(myDate.toLocaleString());
+//  console.log(myDate.toTimeString());
+
+let myCreateDate=new Date(2024,0,24,10,30,55)
+console.log(myCreateDate.toLocaleString());
+
+
+let mydate=new Date()
+console.log(mydate.getMonth()+1);
+console.log(mydate.getFullYear());
+console.log(mydate.getDay());
+
+
+
+
+
+ 
