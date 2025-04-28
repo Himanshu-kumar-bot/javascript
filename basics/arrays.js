b9a656bdb64a9194f6c23e2cace52a3f27ef54c5
Himanshu Kumar <dellvastro06@gmail.com>
2025-04-28 19:05:13 +0000

    Arrays_02

--- basics/arrays.js
+++ basics/arrays.js
@@ -34,10 +34,52 @@ const myNarr=new Array(0,1,2,3,4,5)
  
   
  const myNarr4 =myNarr.splice(1,3)
- 
- console.log(myNarr);   //[ 0, 4, 5 ]
+
+//  console.log(myNarr);   //[ 0, 4, 5 ]
                        // [ 1, 2, 3 ]
- console.log(myNarr4);
+//  console.log(myNarr4);
+
+
+//Push -> push in same array and modify it.
+const number=[1,2,3,4]
+const alphabet=[5,6,7,8]
+number.push(alphabet)
+// console.log(number)  // [ 1, 2, 3, 4, [ 5, 6, 7, 8 ] ]
+// console.log(number[4][2]) //7
+
+
+
+//concat -> Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+
+const number1=[1,2,3,4]
+const alphabet1=[5,6,7,8]
+//  const newArray=number1.concat(alphabet1)
+//  console.log(newArray);  // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+ 
+//spread
+
+const mynewNo=[...number1,...alphabet1]
+// console.log(mynewNo);
+
+
+const anotherArray=[1,2,3,4,5,[6,7,8,9],10,11,[12,13,14,[15,16,17,[18,19,20]]]]
+const real_another_array=anotherArray.flat(Infinity)
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Himanshu"))
+console.log(Array.from("Himanshu"))
+console.log(Array.bind("Himanshu"))
+
+console.log(Array.from({name: "hitesh"})) // interesting
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
  
  
  
